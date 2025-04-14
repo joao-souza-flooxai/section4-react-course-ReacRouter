e8fc4ba857360db0b66b5fbc9f4bb3852a3d9522
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-14 15:40:38 -0300

    feat: Added the param and not found page and route. Finished the course.

--- navegation/src/components/layout/Menu.jsx
+++ navegation/src/components/layout/Menu.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import "./Menu.css"
+import {Link} from "react-router-dom";
 
 const Menu = (props) =>{
     return (
         <aside className="Menu">
             <nav>
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li> 
+                    <li><Link to="/param/123">Param #01</Link></li>
+                    <li><Link to="/param/legal">Param #02</Link></li> 
+                    <li><Link to="/notFound">Not Found</Link></li> 
                 </ul>
             </nav>
         </aside>
---
+++ navegation/src/views/examples/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = props => (
+    <div className="NotFound">
+        <h1>404</h1>
+        <h2>Oops... Páge not Found!</h2>
+    </div>
+);
+
+export default NotFound;
